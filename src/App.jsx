e98c2463ab676e5315ec9663aa9ec4754fe971d6
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-04-28 15:04:06 +0530

    Fixed Banner image path again

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const speakerPageDetails = [
     instaLink: "https://www.instagram.com/dr.tanujain/",
     linkedinLink: "https://www.linkedin.com/in/dr-tanu-jain-4aa28a131",
     twittLink: "https://twitter.com/DrTanuJain1",
-    BannerURL: "public/banner/banner_img.png",
+    BannerURL: "/banner/banner_img.png",
     achivements: [
       "Former IAS officer of 2015 batch.",
       "Served her excellence as Assistant Director of DRDO.",
